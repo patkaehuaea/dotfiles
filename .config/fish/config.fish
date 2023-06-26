196bfa1b2567be8902eca921215b8708d164b21f
Pat Kaehuaea <>
2023-06-26 12:31:00 -0700

    fixed go path in fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 # ~/.shellpaths
 # add homebrew path for Apple silicon 
 fish_add_path /opt/homebrew/bin
-fish_add_path /users/pat/go
+fish_add_path /users/pat/go/bin
 
 # ~~/.shellvars
 set -x EDITOR vim
