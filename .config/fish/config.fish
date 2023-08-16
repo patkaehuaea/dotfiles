5a355ab9b6fadb594b4fd1aab313d58021b60e3a
Pat Kaehuaea <>
2023-08-15 19:20:17 -0700

    blah

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -58,3 +58,7 @@ starship init fish | source
 
 # direnv shell hook
 direnv hook fish | source
+
+# Setting PATH for Python 3.10
+# The original version is saved in /Users/pat/.config/fish/config.fish.pysave
+set -x PATH "/Library/Frameworks/Python.framework/Versions/3.10/bin" "$PATH"
